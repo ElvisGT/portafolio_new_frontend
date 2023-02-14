78bbfaec2eca8361fa91f8c0408b7748e3d1e6c3
ElvisGT <elvisgiltorres@gmail.com>
2023-02-14 17:12:48 -0500

    Reajustando medidas en el boton de stacks

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 import { buttonProps } from "../types"
 
 export const Button = ({ handleStack, text,customClass}: buttonProps) => {
-	const activeButtonStyles = 'bg-alert  min-w-20  w-24 h-8 text-white rounded-xl  m-2'
+	const activeButtonStyles = 'bg-alert px-4 w-auto h-8 text-white rounded-xl  m-4'
 
     return (
         <button onClick={handleStack}
             className={customClass ? activeButtonStyles : 
-            "bg-main w-auto px-4 min-h-8 h-8 text-white rounded-xl m-2 shadow-xl"}>
+            "bg-main w-auto px-4 min-h-8 h-8 text-white rounded-xl m-4 shadow-xl"}>
             {text}
         </button>
     )
