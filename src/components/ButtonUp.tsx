13eec2c2a22dd4ccb2ea8c15e4bd12352dee564b
ElvisGT <elvisgiltorres@gmail.com>
2023-02-14 16:49:37 -0500

    Flecha para ir al top de proyectos

---
+++ src/components/ButtonUp.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export const ButtonUp = () => {
+	const screenHeight = 500;
+	const [button, setButton] = useState(false)
+
+	useEffect(() => {
+		document.addEventListener("scroll",() => {
+			if(document.documentElement.scrollTop > screenHeight){
+				setButton(true)
+			}else {
+				setButton(false)
+			}
+		})
+	},[]) 
+	const handleUp = () => {
+		window.scroll(0,0);
+	}
+	return (
+		<>
+			{button && 
+				<img onClick={handleUp}
+					className={'fixed bottom-5 right-6 z-20'} 
+					src="https://i.ibb.co/hfnp5g2/icons8-up-arrow-60-1.png"
+					alt="flecha arriba"/>
+				
+			}
+		</>
+	)
+}
